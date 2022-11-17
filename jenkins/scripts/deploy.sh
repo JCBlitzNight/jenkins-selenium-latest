2c10d6e4b5a629308dd79695110a224a4a5b388a
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2022-11-17 13:06:50 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/lab07b-php-automatedUITesting/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/Lab7b_Selenium_Raymond/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
